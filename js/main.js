8e91fae14467c07ce83316a0853fcf6d3a8c900b
Andi <galynski@yahoo.com>
2020-12-28 00:51:43 +0300

    ok

---
+++ js/main.js
@@ -0,0 +1,30 @@
+function click() {
+  let input = document.querySelector('.content__input');
+  // let out = document.querySelector('.content__out');
+  let val = input.value;
+      val = parseInt(val);
+
+  if (isNaN(val)) {
+    alert('Введите цифру от 0 до 10');
+  }
+  else {
+    if (val == random) {
+      out = document.querySelector('.content__out-1 img').style.display = 'block';
+      
+      setTimeout(location.reload.bind(location), 10000);
+    }
+    else {
+      alert('Не верно!!!');
+      input.value = '';
+      // out = document.querySelector('.content__out-2 img').style.display = 'block';
+      // setTimeout(location.reload.bind(location), 3000);
+    }
+  }
+}
+
+document.querySelector('.btn').onclick = click;
+
+let random = Math.random();
+    random = random * 10;
+    random = Math.round(random);
+    console.log(random);
